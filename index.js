37144f376f82a40b87b870dc95ef7d3311a5fdef
Apiwch <129593511+Apiwch@users.noreply.github.com>
2023-11-21 11:45:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,9 +31,11 @@ app.post('/api/SLM', function (req, res) {
 	let database = `SLM`
     const SL = req.query.SLM;
     const T = req.query.Ti;
+    console.log(T);
+    const timestamp = new Date().toLocaleTimeString();
     const SLRecord = {
         SL: SL,
-        timestamp: T
+        timestamp: timestamp
     };
     if (data.length >= 100) {
         // Remove the first element from the array
@@ -42,7 +44,7 @@ app.post('/api/SLM', function (req, res) {
     const points =
     [
         Point.measurement("SL")
-            .setFloatField("SLV", SL)
+            .setIntegerField("SLV", SL)
     ];
     for (let i = 0; i < points.length; i++) {
         const point = points[i];
@@ -52,11 +54,39 @@ app.post('/api/SLM', function (req, res) {
     }
     
     data.push(SLRecord);
-	res.send('success : ' + req.query.SLM)
+	res.send('success : ' + req.query.SLM +' '+ req.query.Ti)
 })
 
 app.get('/api/SLM', function (req, res) {
     res.json(data);
+});
+
+app.get('/api/His', function (req, res){
+    let dateNow = new Date().toISOString().split('T')[0];
+    var data = [];
+    async function main() {
+        const query = `SELECT *
+        FROM "SL"
+        WHERE
+        time >= timestamp '${dateNow}T01:00:00.000Z' AND time <= timestamp '${dateNow}T13:00:00.000Z'`;
+        const rows = await client.query(query, 'SLM');
+
+        for await (const row of rows) {
+            let ants = row.SLV || '';
+            let time = new Date(row.time);
+            
+            var SlvHis ={
+                SlvH: ants.toString(),
+                timeH: time.toLocaleTimeString('en-GB'),
+            } 
+            data.push(SlvHis);
+        }
+
+        res.json(data);
+    }
+    
+    main()
+    
 });
 
 app.listen(app.get('port'), function () {
